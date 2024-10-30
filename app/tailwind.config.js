0be95a5d3bf46d24be24d6f81b633c41fd76c167
mirorac <racmiroslav@gmail.com>
2024-10-31 00:58:33 +0100

    update dependencies

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
   ],
 }
